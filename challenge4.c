28c5f0660398347997fff55c6a2f64d080c25239
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-09-04 17:28:19 +0100

    exercice4

--- challenge4.c
+++ challenge4.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-// Challenge 5 : Trouver le Minimum
-// Écrivez un programme C qui trouve et affiche le plus petit élément dans un tableau d'entiers.
-//  Le programme doit demander le nombre d'éléments et les éléments du tableau.
+// Challenge 4 : Trouver le Maximum
+// Écrivez un programme C qui trouve et affiche le plus grand élément dans un tableau d'entiers.
+// Le programme doit demander le nombre d'éléments et les éléments du tableau
 
 int main() {
     int longeur;
-    printf("enter le nombre d'elements\n");
+    printf("entrer le nombre d'elements\n");
     scanf("%i", &longeur);
-    int nombres[]
+    int nombres[longeur];
+    int helper;
+    for (size_t i = 0; i < longeur; i++)
+    {
+        printf("ecrire le nombre %i:\n", i + 1);
+        scanf("%i", nombres[i]);
+        if(nombres[i] ==)
+    }
+    
 }
