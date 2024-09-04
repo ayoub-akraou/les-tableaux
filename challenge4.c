80a1f2d26de83692b51ba48ae6b5875ce5935768
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-09-04 22:26:30 +0100

    Update challenge4.c
    continuation du challenge

--- challenge4.c
+++ challenge4.c
@@ -3,9 +3,22 @@
 // Écrivez un programme C qui trouve et affiche le plus petit élément dans un tableau d'entiers.
 //  Le programme doit demander le nombre d'éléments et les éléments du tableau.
 
-int main() {
+int main()
+{
     int longeur;
     printf("enter le nombre d'elements\n");
     scanf("%i", &longeur);
-    int nombres[]
+    int nombres[longeur];
+    int max = 0;
+    
+    for (int i = 0; i < longeur; i++) {
+        printf("saisi le nombre %i:\n", i + 1);
+        scanf("%i", &nombres[i]);
+        if(max < nombres[i]) {
+            max = nombres[i];
+        }
+    }
+    printf("le nombre max: %i", max);
+    
+    return 0;
 }
